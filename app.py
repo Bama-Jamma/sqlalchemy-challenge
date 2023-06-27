06328e984524fbccf52ecfbc1914ccc2494bb5d8
Bama-Jamma <dustin.shaddix@gmail.com>
2023-06-27 17:46:42 -0400

    updated code
    changed formatting using CSS and used a call to openweatherAPI to get current weather for Hawaii

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import requests
 from flask import Flask, jsonify
 import datetime as dt
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 import numpy as np
+from config import api_key
+
+
 
 #################################################
 # Database Setup
@@ -28,20 +32,103 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
+    base_url = "https://api.openweathermap.org/data/2.5/weather?"
+    lat = 21.3069  # Latitude for Hawaii
+    lon = -157.8583  # Longitude for Hawaii
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": api_key,
+        "units": "metric"  # Request temperature in Celsius
+    }
+    response = requests.get(base_url, params=params)
+    
+    if response.status_code == 200:
+        weather_data = response.json()
+        current_temp_c = weather_data["main"]["temp"]
+        current_temp_f = (current_temp_c * 9/5) + 32
+        condition = weather_data["weather"][0]["description"]
+    else:
+        current_temp_c = "N/A"
+        current_temp_f = "N/A"
+        condition = "N/A"
+
     return (
-        f"Welcome to the Hawaii Climate Analysis API!<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"Returns a JSON list of precipitation data for the dates between 8/23/2016 and 8/23/2017<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"Returns a JSON list of stations from the dataset<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"Returns a JSON list of temperature observations (TOBS) for the dates between 8/23/2016 and 8/23/2017<br/>"
-        f"/api/v1.0/&lt;start&gt;<br/>"
-        f"Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for all dates greater than and equal to the start date<br/>"
-        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
-        f"Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for the dates between the start and end dates (inclusive)<br/>"
-    )
+        """
+        <html>
+        <head>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    background-color: #f2f2f2;
+                    color: #333333;
+                    margin: 50px;
+                }}
+                
+                h1 {{
+                    font-size: 36px;
+                    font-weight: bold;
+                    margin-bottom: 20px;
+                }}
+                
+                h2 {{
+                    font-size: 24px;
+                    font-weight: bold;
+                    margin-top: 30px;
+                    margin-bottom: 10px;
+                }}
+                
+                h3 {{
+                    font-size: 18px;
+                    font-weight: bold;
+                    margin-top: 20px;
+                    margin-bottom: 10px;
+                }}
+                
+                p {{
+                    font-size: 16px;
+                    line-height: 1.5;
+                    margin-bottom: 15px;
+                }}
+                
+                a {{
+                    color: #007bff;
+                    text-decoration: none;
+                }}
+                
+                a:hover {{
+                    text-decoration: underline;
+                }}
+            </style>
+        </head>
+        <body>
+            <h1>Welcome to the Hawaii Climate Analysis API!</h1>
+            <p>This API provides climate analysis for the beautiful islands of Hawaii.</p>
+            
+            <h2>Current Weather in Hawaii:</h2>
+            <p>Temperature: {}°C / {}°F</p>
+            <p>Condition: {}</p>
+            
+            <h2>Available Routes:</h2>
+            
+            <h3><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></h3>
+            <p>Returns a JSON list of precipitation data for the dates between 8/23/2016 and 8/23/2017.</p>
+            
+            <h3><a href="/api/v1.0/stations">/api/v1.0/stations</a></h3>
+            <p>Returns a JSON list of stations from the dataset.</p>
+            
+            <h3><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></h3>
+            <p>Returns a JSON list of temperature observations (TOBS) for the dates between 8/23/2016 and 8/23/2017.</p>
+            
+            <h3><a href="/api/v1.0/<start>">/api/v1.0/&lt;start&gt;</a></h3>
+            <p>Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for all dates greater than and equal to the start date. Replace &lt;start&gt; in the URL with a valid date in the format YYYY-MM-DD.</p>
+            
+            <h3><a href="/api/v1.0/<start>/<end>">/api/v1.0/&lt;start&gt;/&lt;end&gt;</a></h3>
+            <p>Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for the dates between the start and end dates (inclusive). Replace &lt;start&gt; and &lt;end&gt; in the URL with valid dates in the format YYYY-MM-DD.</p>
+        </body>
+        </html>
+        """.format(current_temp_c, current_temp_f, condition))
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
---
+++ config.py
@@ -0,0 +1,1 @@
+api_key = "8f3b5c6348f83db207d070bd94fc8629"
